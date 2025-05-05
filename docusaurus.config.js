e22d3ae6095f097a22b207492f186b418c48f44a
Muhammed Fatih Bayraktar <mfbayraktar@live.com>
2025-05-05 22:28:21 +0200

    css

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -93,7 +93,7 @@ const config = {
       },
       colorMode: {
         defaultMode: 'dark',
-        disableSwitch: false,
+        disableSwitch: true,
         respectPrefersColorScheme: false,
       },
       footer: {
